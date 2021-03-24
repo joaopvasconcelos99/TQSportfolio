10a59a0d65742720ff7db5017caaa5bd28b57ebb
João Vasconcelos <joaopvasconcelos@ua.pt>
2021-03-24 17:16:16 +0000

    finished lab02

--- Lab02/P2gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
+++ Lab02/P2gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -9,44 +9,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.contains;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 public class AddressResolverIT {
-
-    @Mock
-    ISimpleHttpClient client;
-
-    @InjectMocks
-    AddressResolver resolver;
 
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
 
-        resolver = new AddressResolver(new TqsBasicHttpClient());
-
-        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
-
-        assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
-
-    }
-
-    @Test
-    public void whenGoodCoordidates_returnAddress2() throws IOException, URISyntaxException, ParseException {
-
-        String jsonResponse = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=1739540548\",\"roadMetadata\":null}]}]}";
-
-        when(client.get(contains("location=40.640661%2C-8.656688"))).thenReturn(jsonResponse);
+        AddressResolver resolver = new AddressResolver(new TqsBasicHttpClient());
 
         Address result = resolver.findAddressForLocation(40.640661, -8.656688);
 
@@ -57,9 +32,7 @@ public class AddressResolverIT {
     @Test
     public void whenBadCoordidates_trhowBadArrayindex() throws IOException, URISyntaxException, ParseException {
 
-        String jsonResponse = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2020 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2020 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":100.0,\"lng\":100.0}},\"locations\":[]}]}";
-
-        when(client.get(anyString())).thenReturn(jsonResponse);
+        AddressResolver resolver = new AddressResolver(new TqsBasicHttpClient());
 
         assertThrows(IndexOutOfBoundsException.class, new Executable() {
             @Override
